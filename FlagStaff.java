e2734dce0cf9db6aa196a1c3ffe613c099c84330
Nathan Collinson <nathan.collinson@student.onslow.school.nz>
2020-05-06 12:55:36 +1200

    Initial sharing of project

---
+++ FlagStaff.java
@@ -0,0 +1,73 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a Onslow College 13DTC assignment.
+// You may not distribute it in any other way without permission.
+
+/* Exercise for Onslow College 13DTC
+ * Name:
+ * Email:
+ * Date:
+ */
+import ecs100.*;
+import java.awt.Color;
+
+/** A FlagStaff object represents a flag on a flagstaff.
+ *   The flagstaff has a pole that is FLAGSTAFF_HEIGHT units high.
+ *   It has a flag whose position can be anywhere from the bottom of the pole to the top
+
+ *   The constructor specifies the position of the base of the flagstaff.
+
+ *   The bottom left corner of the flag is initially at the bottom of the pole.
+ *   The flag is FLAG_SIZE units high.
+ *   The flag can be raised or lowered.
+ *
+ *   The flag can be as simple as a blue square, but you can draw a more fancy flag if you want
+ *
+ *   The class has three methods:
+ *     - raise(double dist), which changes the flag position on the staff but does not let it go over the top of the flagstaff
+ *     - lower(double dist), which changes the flag position on the staff but does not let it go below the bottom of the flagstaff.
+ *           Both methods change the field, but do not redraw the flag.
+ *     - redraw() which draws the flagstaff [UI.drawLine(....)] and the flag [UI.fillRect(....)]
+ */
+public class FlagStaff{
+
+    public static final double FLAGSTAFF_HEIGHT = 300;  // 
+    public static final double FLAG_SIZE = 30;  //
+
+    // fields,
+    // position of the base of the flag staff
+    // position of the flag (eg, the position of the bottom of the flag)
+    // position of the ground
+    /*# YOUR CODE HERE */
+
+    /** Constructor:
+     * Parameters: the position of the base of the flag staff
+     * Initialises the fields, but does not draw the flag.
+     */
+    /*# YOUR CODE HERE */
+
+    /** redraw method:
+     * Draws the flagstaff (a line) and the flag (a blue rectangle)*/
+    /*# YOUR CODE HERE */     
+
+    /** raise method:
+     * Parameter:  amount to raise the flag
+     * Changes the field, but does not let it go over the top of the flagstaff.
+     * Does not redraw the flag.
+     */
+    /*# YOUR CODE HERE */
+
+    /** lower method:
+     * Parameter:  amount to lower the flag
+     * Changes the field, but does not let it go below the bottom of the flagstaff.
+     * Does not redraw the flag.
+     */
+    /*# YOUR CODE HERE */
+
+    /** printCountry method:
+     * Parameter text to print to screen
+     * Prints at the base of the Flag
+     */
+    /*# YOUR CODE HERE */
+
+}
+
---
+++ FlagStaffExercise.java
@@ -0,0 +1,100 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a Onslow College 13DTC assignment.
+// You may not distribute it in any other way without permission.
+
+/* Exercise for Onslow College 13DTC
+ * Name:
+ * Email:
+ * Date:
+ */
+
+import ecs100.*;
+import java.awt.Color;
+
+/** Exercise for defining objects.
+ *  This program contains methods for testing Lamp, PartyBalloon, and FlagStaff objects.
+ *  It is all written for you, but you need to read it to see what the
+ *  Lamp, PartyBalloon, and FlagStaff classes should do
+ */
+
+public class FlagStaffExercise{
+
+    private static final double GROUND = 400;
+    private FlagStaff flag1 = new FlagStaff(110, GROUND); 
+
+    /** 
+     * Makes two FlagStaff objects and makes the flags go up and down.
+     */
+    public void doFlagStaff(){
+        this.clear();
+
+        this.drawGround();
+        flag1.redraw();
+        UI.sleep(500);
+
+        int steps = 0;
+        UI.println("Make flags go up");
+        while (steps < 8){
+            // make flags go up by a random amount 
+            flag1.raise(Math.random()*100);
+
+            UI.clearGraphics();
+            this.drawGround();
+            flag1.redraw();
+
+            UI.sleep(500);
+            steps++;   //(shorthand for steps = steps + 1;
+        }
+        UI.println("Make flags go down");
+        while (steps> 0){
+            // make flags go down by a random fraction
+            flag1.lower(Math.random()*100);
+
+            UI.clearGraphics();
+            this.drawGround();
+            flag1.redraw();
+
+            UI.sleep(500);
+            steps--;   //(shorthand for steps = steps - 1;
+        }
+    }
+
+    /** Draw the ground */
+    public void drawGround(){
+        UI.setColor(new Color(120, 80, 0));   // dark brown,
+        UI.fillRect(0,GROUND,600,10);  
+    }
+
+    public void clear(){
+        UI.clearPanes();
+    }
+
+    /** doRaise method:
+     * raise the flag*/
+    /*# YOUR CODE HERE */
+    
+    /** doLower method:
+     * lower the flag*/
+    /*# YOUR CODE HERE */     
+        
+    /** setAmount method:
+     * sets the amount to raise or lower flag by*/
+    /*# YOUR CODE HERE */     
+    
+    /** printCountry method:
+     * print the country the user entered in textfield*/
+    /*# YOUR CODE HERE */ 
+    
+    // Main
+    /** Create a new BallGame object and setup the interface */
+    public static void main(String[] args){
+        FlagStaffExercise fse = new FlagStaffExercise();
+        UI.addButton("Clear", fse::clear );
+        UI.addButton("FlagStaff", fse::doFlagStaff );
+        
+        // Add buttons, textfield, slider
+        /*# YOUR CODE HERE */
+        UI.addButton("Quit", UI::quit );        
+    }
+
+}
